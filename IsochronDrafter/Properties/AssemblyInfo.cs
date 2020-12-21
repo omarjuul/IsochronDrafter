ff27751bd96270b342f252a2c73390e87805ec71
Camiel Egbers <camiel.egbers@gmail.com>
2020-12-21 17:36:32 +0100

    AssemblyTitle -> 'IsoCubeDrafter'

--- IsochronDrafter/Properties/AssemblyInfo.cs
+++ IsochronDrafter/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IsochronDrafter")]
+[assembly: AssemblyTitle("IsoCubeDrafter")]
 [assembly: AssemblyDescription("Draft cubes with friends")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
